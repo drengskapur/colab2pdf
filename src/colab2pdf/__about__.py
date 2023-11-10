9b26680d2903fdd214e55fcdf34dbda8ef21d42b
jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>
2023-11-10 06:00:44 -0500

    Update __about__.py
    Signed-off-by: jonathanagustin <5193877+jonathanagustin@users.noreply.github.com>

--- src/colab2pdf/__about__.py
+++ src/colab2pdf/__about__.py
@@ -1,4 +1,4 @@
 # SPDX-FileCopyrightText: 2023-present Drengskapur <service@drengskapur.com>
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-__version__ = "1.0.0-alpha.14"
+__version__ = "1.0.2-alpha.14"
